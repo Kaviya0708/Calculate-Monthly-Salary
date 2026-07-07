f9e7b0e68172dcdc8a21d47bc76c682dbdb66002
Kaviya <732125104051.25.cse@nandhatech.org>
2026-07-07 06:06:10 +0530

    Create Main17.java

---
+++ Main17.java
@@ -0,0 +1,8 @@
+class Main {
+    public static void main(String[] args) {
+        int dailywage=800;
+        int workingdays=25;
+        int salary=dailywage*workingdays;
+        System.out.println("Total salary "+salary);
+    }
+}
